5ea576f85955fd9cd1bd77033fc22f125170b74b
ddmxm <korachev@gmail.com>
2017-02-02 15:55:04 +0300

    Пройдены все задания до 16 уровня 7 урока.

--- src/com/javarush/test/level16/lesson07/task05/Solution.java
+++ src/com/javarush/test/level16/lesson07/task05/Solution.java
@@ -5,32 +5,40 @@ package com.javarush.test.level16.lesson07.task05;
 2. Исправь метод takingOff(взлет) - сейчас он работает оооочень долго. Взлет должен занимать 100 миллисекунд.
 3. Реализуй метод waiting по аналогии с методом takingOff. Время ожидания не должно превышать время взлета.
 */
-public class Solution {
+public class Solution
+{
     public static volatile Runway RUNWAY = new Runway();   //1 взлетная полоса
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException
+    {
         Plane plane1 = new Plane("Самолет #1");
         Plane plane2 = new Plane("Самолет #2");
         Plane plane3 = new Plane("Самолет #3");
     }
 
-    public static class Plane extends Thread {
-        public Plane(String name) {
+    public static class Plane extends Thread
+    {
+        public Plane(String name)
+        {
             super(name);
             start();
         }
 
-        public void run() {
-            boolean  isAlreadyTakenOff = false;
-            while (!isAlreadyTakenOff) {
-                if (RUNWAY.getTakingOffPlane() == null) {    //если взлетная полоса свободна
+        public void run()
+        {
+            boolean isAlreadyTakenOff = false;
+            while (!isAlreadyTakenOff)
+            {
+                if (RUNWAY.getTakingOffPlane() == null)
+                {    //если взлетная полоса свободна
                     RUNWAY.setTakingOffPlane(this);
                     System.out.println(getName() + " взлетает");
                     takingOff();//взлетает
                     System.out.println(getName() + " уже в небе");
                     isAlreadyTakenOff = true;
                     RUNWAY.setTakingOffPlane(null);
-                } else if (!this.equals(RUNWAY.getTakingOffPlane())) {  //если взлетная полоса занята самолетом
+                } else if (!this.equals(RUNWAY.getTakingOffPlane()))
+                {  //если взлетная полоса занята самолетом
                     System.out.println(getName() + " ожидает");
                     waiting(); //ожидает
                 }
@@ -38,7 +46,8 @@ public class Solution {
         }
     }
 
-    private static void waiting() {
+    private static void waiting()
+    {
         //add your code here - добавь код тут
         try
         {
@@ -50,23 +59,31 @@ public class Solution {
         }
     }
 
-    private static void takingOff() {
+    private static void takingOff()
+    {
         //fix this method - исправь этот метод
-        try {
+        try
+        {
             Thread.sleep(100);
-        } catch (InterruptedException e) {
+        }
+        catch (InterruptedException e)
+        {
         }
     }
 
-    public static class Runway { //взлетная полоса
+    public static class Runway
+    { //взлетная полоса
         private Thread t;
 
-        public Thread getTakingOffPlane() {
+        public Thread getTakingOffPlane()
+        {
             return t;
         }
 
-        public void setTakingOffPlane(Thread t) {
-            synchronized (this) {
+        public void setTakingOffPlane(Thread t)
+        {
+            synchronized (this)
+            {
                 this.t = t;
             }
         }
